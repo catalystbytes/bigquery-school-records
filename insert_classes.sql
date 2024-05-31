ea04f4d87dbec472b91517181bdc986eedec1d6e
catalystbytes <153075269+catalystbytes@users.noreply.github.com>
2024-05-31 17:51:32 +0800

    Create insert_classes.sql

---
+++ insert_classes.sql
@@ -0,0 +1,7 @@
+-- Insert data into Classes Table
+INSERT INTO `dbt-learn-360010.school_records.Classes` 
+(class_id, class_name, teacher_id, schedule)
+VALUES
+  (101, 'Algebra I', 1, 'Mon-Wed-Fri 09:00-10:00'),
+  (102, 'Biology', 2, 'Tue-Thu 10:00-11:30'),
+  (123, 'World History', 12, 'Mon-Wed-Fri 11:00-12:00');
