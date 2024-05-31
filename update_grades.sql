af86d95675f717db7570bd12bb2e328a5bbbade5
catalystbytes <153075269+catalystbytes@users.noreply.github.com>
2024-05-31 18:04:57 +0800

    Update update_grades.sql

--- update_grades.sql
+++ update_grades.sql
@@ -2,7 +2,9 @@
 UPDATE `dbt-learn-360010.school_records.Grades`
 SET
   grade = CASE 
-              WHEN grade = 4 THEN 5
-            END
+    WHEN grade is not null THEN 5
+    WHEN grade is null THEN 5
+    ELSE grade
+  END
 WHERE 
   grade_id IN (2532, 1152);
