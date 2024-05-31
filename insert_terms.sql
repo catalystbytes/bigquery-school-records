bbcd7c7f125f6e6ec70961eae7402fac09694f8f
catalystbytes <153075269+catalystbytes@users.noreply.github.com>
2024-05-31 17:51:52 +0800

    Create insert_terms.sql

---
+++ insert_terms.sql
@@ -0,0 +1,9 @@
+-- Insert data into Terms Table
+INSERT INTO `dbt-learn-360010.school_records.Terms` 
+(term_id, term_name, start_date, end_date)
+VALUES
+  ('2021T1', 'Term 1 2021', '2021-01-01', '2021-04-30'),
+  ('2022T1', 'Term 1 2022', '2022-01-01', '2022-04-30'),
+  ('2022T2', 'Term 2 2022', '2022-05-01', '2022-08-31'),
+  ('2022T3', 'Term 3 2022', '2022-09-01', '2022-12-31'),
+  ('2023T1', 'Term 1 2023', '2023-01-01', '2023-04-30');
