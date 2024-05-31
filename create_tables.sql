18f672fe711eb88d5864ea3d31ca7ef3297d1c9c
catalystbytes <153075269+catalystbytes@users.noreply.github.com>
2024-05-31 18:10:46 +0800

    Update create_tables.sql

--- create_tables.sql
+++ create_tables.sql
@@ -1,5 +1,6 @@
--- SQL to create all tables
-CREATE TABLE `dbt-learn-360010.school_records.Students` (
+-- SQL to create or replace all tables
+
+CREATE OR REPLACE TABLE `dbt-learn-360010.school_records.Students` (
   student_id INT64,
   first_name STRING,
   last_name STRING,
@@ -10,7 +11,7 @@ CREATE TABLE `dbt-learn-360010.school_records.Students` (
   status STRING
 );
 
-CREATE TABLE `dbt-learn-360010.school_records.Teachers` (
+CREATE OR REPLACE TABLE `dbt-learn-360010.school_records.Teachers` (
   teacher_id INT64,
   first_name STRING,
   last_name STRING,
@@ -20,21 +21,21 @@ CREATE TABLE `dbt-learn-360010.school_records.Teachers` (
   department STRING
 );
 
-CREATE TABLE `dbt-learn-360010.school_records.Classes` (
+CREATE OR REPLACE TABLE `dbt-learn-360010.school_records.Classes` (
   class_id INT64,
   class_name STRING,
   teacher_id INT64,
   schedule STRING
 );
 
-CREATE TABLE `dbt-learn-360010.school_records.Terms` (
+CREATE OR REPLACE TABLE `dbt-learn-360010.school_records.Terms` (
   term_id STRING,
   term_name STRING,
   start_date DATE,
   end_date DATE
 );
 
-CREATE TABLE `dbt-learn-360010.school_records.Grades` (
+CREATE OR REPLACE TABLE `dbt-learn-360010.school_records.Grades` (
   grade_id INT64,
   student_id INT64,
   class_id INT64,
